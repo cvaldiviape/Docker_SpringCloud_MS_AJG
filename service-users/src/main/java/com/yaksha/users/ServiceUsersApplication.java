2b91e20fe27c5656924c279289971a0f67937952
cvaldiviape <cvaldiviape@gmail.com>
2023-06-10 18:55:29 -0500

    add service-users

---
+++ service-users/src/main/java/com/yaksha/users/ServiceUsersApplication.java
@@ -0,0 +1,13 @@
+package com.yaksha.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceUsersApplication.class, args);
+	}
+
+}
---
+++ service-users/src/test/java/com/yaksha/users/ServiceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaksha.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
