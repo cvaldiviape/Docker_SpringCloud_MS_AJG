51c36a1aca242b3329514e81674b15fac60cfa25
cvaldiviape <cvaldiviape@gmail.com>
2023-06-10 20:34:12 -0500

    add service courses

---
+++ service-courses/src/main/java/com/yaksha/ServiceCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.yaksha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCoursesApplication.class, args);
+	}
+
+}
---
+++ service-courses/src/test/java/com/yaksha/ServiceCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
