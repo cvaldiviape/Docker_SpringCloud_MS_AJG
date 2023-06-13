25525e9f24c459d4afb55ab92717d16685e4b14e
cvaldiviape <cvaldiviape@gmail.com>
2023-06-12 23:41:35 -0500

    config docker service-users

--- service-users/src/main/java/com/yaksha/users/client/CourseFeignClient.java
+++ service-users/src/main/java/com/yaksha/users/client/CourseFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="service-courses", url="localhost:8002/api/courses")
+@FeignClient(name="service-courses", url="host.docker.internal:8002/api/courses")
 public interface CourseFeignClient {
 
     @DeleteMapping("/delete-courseUser/{id}")
