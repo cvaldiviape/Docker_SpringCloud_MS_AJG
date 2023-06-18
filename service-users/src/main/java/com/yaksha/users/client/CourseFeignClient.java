33c69c0e77166583b50f3788522af74a24df76ee
cvaldiviape <cvaldiviape@gmail.com>
2023-06-17 21:23:14 -0500

    modify clients

--- service-users/src/main/java/com/yaksha/users/client/CourseFeignClient.java
+++ service-users/src/main/java/com/yaksha/users/client/CourseFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="service-courses", url="host.docker.internal:8002/api/courses")
+@FeignClient(name="service-courses", url="service-courses:8002/api/courses") // es buena practica que el hostname "service-courses" se llame igual al mi servicio "service-courses"
 public interface CourseFeignClient {
 
     @DeleteMapping("/delete-courseUser/{id}")
