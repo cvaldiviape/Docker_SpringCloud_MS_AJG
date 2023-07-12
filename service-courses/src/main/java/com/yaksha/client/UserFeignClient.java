26240e1763109335566755a73447210dab78c852
cvaldiviape <cvaldiviape@gmail.com>
2023-07-11 23:14:02 -0500

    init

--- service-courses/src/main/java/com/yaksha/client/UserFeignClient.java
+++ service-courses/src/main/java/com/yaksha/client/UserFeignClient.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@FeignClient(name="service-users", url="${saksa.service.users.url}/api/users")  // es buena practica que el hostname "service-users" se llame igual al mi servicio "service-users"
-@FeignClient(name="service-users") // ahora trabaje de esta forma por que estoy usando el DiscoveryCliente de "Kuberbenetes", ademas tambien instale la dependiente en el "pom.xml"
+@FeignClient(name="service-users", url="${saksa.service.users.url}/api/users")  // es buena practica que el hostname "service-users" se llame igual al mi servicio "service-users"
 public interface UserFeignClient {
 
     @GetMapping("/{id}")
