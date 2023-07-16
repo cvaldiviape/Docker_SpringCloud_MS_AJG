82aae10d19f6f3d35f94624b6a12240eef70e56d
cvaldiviape <cvaldiviape@gmail.com>
2023-07-16 15:13:17 -0500

    config spring cloud gateway

---
+++ service-gateway/src/test/java/com/yaksha/gateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaksha.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
